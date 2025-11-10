c27b7951eedb34cb280673018b68c8f8ee865a81
Kevin Le Perf <codlabtech@gmail.com>
2025-11-10 14:16:55 +0100

    Update for rate limit

--- cex-trading-bot/src/main/kotlin/eu/codlab/cex/Application.kt
+++ cex-trading-bot/src/main/kotlin/eu/codlab/cex/Application.kt
@@ -3,20 +3,48 @@ package eu.codlab.cex
 import eu.codlab.cex.candles.CandleManager
 import eu.codlab.cex.spot.trading.PrivateApi
 import eu.codlab.cex.spot.trading.PublicApi
+import eu.codlab.cex.spot.trading.calls.ApiConfiguration
+import eu.codlab.cex.spot.trading.calls.RateLimitQueue
+import eu.codlab.cex.spot.trading.rest.RestOptions
 import eu.codlab.cex.ticks.TickManager
 import eu.codlab.cex.utils.Looper
 import eu.codlab.cex.wallet.WalletsManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 fun main() {
     runBlocking {
+        val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
         val configuration = Environment.init()
-        val publicApi = PublicApi()
+
+        val apiConfiguration = ApiConfiguration(
+            rateLimitQueue = RateLimitQueue(
+                coroutineScope,
+                // currently 30 - and by default but maybe actually required to be 20U
+                apiTokenPoolMaximum = 30U
+            )
+        )
+
+        val publicApi = PublicApi(
+            coroutineScope = coroutineScope,
+            apiConfiguration = apiConfiguration
+        )
+
         val privateApi = PrivateApi(
+            coroutineScope = coroutineScope,
             apiKey = configuration.apiKey,
-            apiSecret = configuration.apiSecret
+            apiSecret = configuration.apiSecret,
+            apiConfiguration = apiConfiguration,
+            restOptions = RestOptions(
+                rateLimitLog = { prefix, text ->
+                    println("$prefix: $text")
+                }
+            )
         )
 
         val tickManager = Looper(
